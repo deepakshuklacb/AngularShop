1e1bcf0fb3264cb346a23a4b072c087db717d4f4
Deepak Shukla <codingbrains17@gmail.com>
2019-08-26 23:48:46 +0530

    initial

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyCaDTXLpQD07tKjJwo8_EB47C3yDmE6LOU",
+  authDomain: "angular-shop-369c7.firebaseapp.com",
+  databaseURL: "https://angular-shop-369c7.firebaseio.com",
+  projectId: "angular-shop-369c7",
+  storageBucket: "",
+  messagingSenderId: "491627940191",
+  appId: "1:491627940191:web:8f9819c7c781a743"
+};
